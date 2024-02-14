0d8feeafbe9ee29fbf0070263072922a8e09d2fb
Quiroz <cristianquiroz2311@gmail.com>
2024-02-14 07:55:11 -0300

    JSX

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Saludar() {
-  return <h1>Hola componente</h1>;
+  const buleano = true;
+
+  return <h1>{buleano ? "verdadero" : "falso"}</h1>
 }
+
 
 root.render(
   <div>
-    <Saludar></Saludar>
+    <Saludar />
   </div>
 );
