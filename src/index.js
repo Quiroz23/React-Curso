99205ec4c659a3de9dcc6ebc935d84af02fbded9
Quiroz <cristianquiroz2311@gmail.com>
2024-02-22 19:31:03 -0300

    Estilos Online bases

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Saludar, Boleano } from "./Saludar";
 import Producto from "./Producto";
 import { Saludar2, Usercard } from "./Saludar2";
 import { Boton } from "./Boton";
+import { CartaTarea } from "./Tarea";
 
 
 
@@ -11,19 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-  {/* <Boton text= "Hola"/> */}
-
-    {/* <Usercard
-      name="Cristian Quiroz"
-      dinero={2000}
-      casado={false}
-      puntos={[12, 4.2, 5, 5.2]}
-      direccion={{ calle: "Las chinchillas", ciudad: "San carlos" }}
-    />
-    <Saludar2 tittle="Hola React" />
-    <Saludar2 tittle="Hola JSX" />
-    <Saludar />
-    <Boleano />
-    <Producto /> */}
+  <CartaTarea ready={false}/>
   </>
 );
